25e4c74f429c2daba236867cb1d7108477967332
Cristiano Lages <cristianolagesf@hotmail.com>
2022-07-24 17:11:55 -0400

    NavBar and top-text

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Button } from '@mui/material'
+import { AppleLogo, GooglePlayLogo } from 'phosphor-react'
 import './App.css'
+import Menu from './components/Menu.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <Menu />
+      <div className="mt-[80px] flex justify-between ml-2">
+        <div className="w-[400px]">
+          <label className="text-6xl font-extrabold font-sans ">
+            Meet new and interesting dogs nearby.
+          </label>
+          <div className="flex gap-4 justify-center mt-8">
+            <a href="#" className="flex gap-2 font-extrabold text-xl text-gray-900 border justify-center items-center bg-gray-300  w-[150px] h-[40px] rounded shadow-lg
+                    hover:contrast-125
+                    active:scale-90 ">
+              <AppleLogo size={22} color="#141414" weight="duotone" />
+              Download
+            </a>
+            <a href="#" className="flex gap-2 font-extrabold text-xl text-gray-900 border justify-center items-center bg-gray-300  w-[150px] h-[40px] rounded shadow-lg
+                    hover:contrast-125
+                    active:scale-90">
+              <GooglePlayLogo size={22} color="#141414" weight="duotone" />
+              Download
+            </a>
+          </div>
+        </div>
+        <div className="h-[864px] w-[534px] bg-bgImage bg-no-repeat bg- rotate-6" >
+          {/* <img src="./assets/images/iphone6.png" className="h-11 w-11" /> */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
+
   )
 }
 
---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+
+const Menu = () => {
+    return (
+        <div className="flex justify-evenly">
+            <a href="#" className="text-5xl font-extrabold font-sans">tindog</a>
+            <div className="flex">
+                <ul className="flex gap-11 font-semibold text-xl self-center">
+                    <li><a>Contact</a></li>
+                    <li><a>Pricing</a></li>
+                    <li><a>Download</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Menu 
